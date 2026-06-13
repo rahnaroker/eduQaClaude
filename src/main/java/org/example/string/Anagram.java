8f5f5b17bb44154d6c3f72bd577380c9b1e3b4cc
rahnaroker <rahnar0ker@gmail.com>
2026-06-14 01:24:15 +0300

    algos anagram new tricks

--- src/main/java/org/example/string/Anagram.java
+++ src/main/java/org/example/string/Anagram.java
@@ -3,11 +3,15 @@ package org.example.string;
 import org.w3c.dom.ls.LSOutput;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Anagram {
 
     static void main(String[] args) {
-        System.out.println(isAnagramSort("go3to", "roog3"));
+//        System.out.println(isAnagramSort("go3to", "roog3"));
+//        System.out.println(isAnagramSortByArray("og", "go3"));
+        System.out.println(isAnagramSortByMap("aabb", "abba"));
     }
 
     static boolean isAnagramSort(String a, String b) {
@@ -22,6 +26,53 @@ public class Anagram {
         Arrays.sort(bChars);
 
         return Arrays.equals(aChars, bChars);
+    }
+
+    static boolean isAnagramSortByArray(String a, String b) {
+        if (a == null || b == null) return false;
+        if (a.length() != b.length()) return false;
+        if (a.equals(b)) return true;
+
+        int[] counts = new int[128];
+        char[] aChars = a.toCharArray();
+        char[] bChars = b.toCharArray();
+
+        for (char c : aChars) {
+            counts[c]++;
+        }
+
+        for (char c : bChars) {
+            counts[c]--;
+        }
+
+        for (int i : counts) {
+            if (i !=0) return false;
+        }
+
+        return true;
+    }
+
+    static boolean isAnagramSortByMap(String a, String b) {
+        if (a == null || b == null) return false;
+        if (a.length() != b.length()) return false;
+        if (a.equals(b)) return true;
+
+        Map<Character, Integer> counts = new HashMap<>();
+        char[] aChars = a.toCharArray();
+        char[] bChars = b.toCharArray();
+
+        for (char c : aChars) {
+            counts.put(c, counts.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : bChars) {
+            counts.put(c, counts.getOrDefault(c, 0) - 1);
+        }
+
+        for (int v : counts.values()) {
+            if (v !=0) return false;
+        }
+        return true;
     }
 
 }
